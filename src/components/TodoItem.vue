1cc269fdc4600c6ae7e592d6fe8c44946e7873a3
Makiavely <fioletmail@gmail.com>
2021-03-11 13:36:23 +0300

    Update TodoItem.vue

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -7,7 +7,7 @@
    </div>
    <div>
      <button @click="pluralize">Plural</button>
-    // <span class="remove-item" @click="removeTodo(index)">
+    // <span class="remove-item" @click="removeTodo(index)"> КОГДА-ТО ПРОПУСТИЛ ЭТО ИЗМЕНЕНИЕ
      <span class="remove-item" @click="removeTodo(todo.id)">
        &times;
      </span>
